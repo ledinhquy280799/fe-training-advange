86b35f420784512146729fdf6bdf1555fae9aadf
ledinhquy <ledinhquy280799@gmail.com>
2021-11-04 09:16:11 +0700

    custom load bootstrap

---
+++ assets/js/vendor.js
@@ -0,0 +1,4 @@
+// @prepros-append "../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse.js";
+// @prepros-append "../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/carousel.js";
+// @prepros-append "../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/transition.js";
+// @prepros-append "../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/modal.js";
