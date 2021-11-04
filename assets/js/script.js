b83438a606d3555a803d4f6c4385c0e92374eb7f
ledinhquy <ledinhquy280799@gmail.com>
2021-11-04 10:54:32 +0700

    update counting

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,68 +25,38 @@ $(document).ready(function () {
         }
     });
 
-    function Counter(selector) {
-        var self = this;
-
-        //get all props from element
-        this.elem = document.querySelector(selector);
-        this.from = parseInt(this.elem.getAttribute('data-from') || 100);
-        this.to = parseInt(this.elem.getAttribute('data-to') || 1000);
-        this.refreshInterval = parseInt(this.elem.getAttribute('data-refresh-interval') || 50);
-        this.speed = parseInt(this.elem.getAttribute('data-speed') || 5000);
-
-        //start counter
-        //if restart(int) is provided
-        //restart counter after finishing with timeout
-        this.start = function (restart) {
-            //base logic, can be improved
-            var diff = self.to - self.from;
-            var steps = self.speed / self.refreshInterval;
-            var step = diff / steps;
-            var i = 1;
-            var interval = setInterval(function () {
-                var change = step * i;
-                if (change <= self.to) {
-                    self.elem.textContent = Math.round(change);
-                    i++;
-                } else {
-                    self.elem.textContent = Math.round(self.to)
-                    clearInterval(interval);
-                    //try to restart counter
-                    if (restart && restart > 0) {
-                        setTimeout(function () {
-                            self.start(restart);
-                        }, restart);
-                    }
-                }
-            }, self.refreshInterval);
+    
+    function increment(elem, finalVal) {
+        var currVal = parseInt(document.getElementById(elem).innerHTML, 10) || 0;
+        if (currVal < finalVal) {
+            currVal++;
+            document.getElementById(elem).innerHTML = currVal;
+        
+            setTimeout(function() {
+            increment(elem, finalVal);
+            }, 40)
         }
-    }
+    };
 
-    //example2
-    var counter1 = new Counter('#web-projects');
-    //dont restart
-    counter1.start(0);
+    $('#web-projects').waypoint(function() {
+        increment("web-projects", 42);
+    }, {offset: '75%'});
+    
+    $('#client').waypoint(function() {
+        increment("client", 123);
+    }, {offset: '75%'});
 
-    //example2
-    var counter2 = new Counter('#client');
-    //dont restart
-    counter2.start(0);
+    $('#winner').waypoint(function() {
+        increment("winner", 15);
+    }, {offset: '75%'});
 
-    //example2
-    var counter3 = new Counter('#winner');
-    //dont restart
-    counter3.start(0);
+    $('#coffee').waypoint(function() {
+        increment("coffee", 99);
+    }, {offset: '75%'});
 
-    //example2
-    var counter3 = new Counter('#coffee');
-    //dont restart
-    counter3.start(0);
-
-    //example2
-    var counter3 = new Counter('#members');
-    //dont restart
-    counter3.start(0);
+    $('#members').waypoint(function() {
+        increment("members", 24);
+    }, {offset: '75%'});
 });
 
 $(window).on('load', function(){
